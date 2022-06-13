afecddcbb56f47c5e68bb26de32c79a19e3c8f31
YW-Statictics <wuwenkai97@163.com>
2022-06-13 11:09:24 +0800

    工程构建

--- core/src/main/java/com/my/rpc/core/server/Server.java
+++ core/src/main/java/com/my/rpc/core/server/Server.java
@@ -7,7 +7,6 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
